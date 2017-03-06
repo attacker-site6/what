7d59d977e9f73c3d03841f89fadabdb3f92ff093
attacker-site6 <account6@attacker-site.com>
2017-03-06 16:45:11 +0200

    Delete my.py

--- my.py
+++
@@ -1,9 +0,0 @@
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
-
-url = 'https://httpbin.org/post' # Set destination URL here
-post_fields = {'foo': 'bar'}     # Set POST fields here
-
-request = Request(url, urlencode(post_fields).encode())
-json = urlopen(request).read().decode()
-print(json)
